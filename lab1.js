19adfa976a7660aa79faadf98be861ff522cc6f4
Do Tuan <emailcuaban@gmail.com>
2026-09-17 15:45:39 +0700

    lab1

--- lab1.js
+++ lab1.js
@@ -3,3 +3,24 @@ console.log('day la lab 1');
 let name ='tuando';
 name = "tuando";
 const age = 20;
+let myName = "hoadv21"; // string
+// boolean: true / false
+let online = true;
+let isActive = true;
+
+// Neu dung thi xay ra
+if (isActive) {
+    console.log("hoat dong", isActive);
+}
+
+// tham tri, tham chieu, dia chi bo nho may tinh
+name = "hoadv21";
+age = 40;
+const a = 10;
+const b = 3;
+console.log(a * b);
+console.log(a / b);
+console.log("Ten toi la: ", name);
+console.log("Tuoi cua toi la: ", age);
+
+// Ham
